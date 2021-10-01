dd638e4548656d4ec3533b78185350b0eb56e994
teshe2008 <teshe2008@hotmail.com>
2021-10-01 15:17:29 +0300

    initial commit

---
+++ database/migrations/2021_09_30_011445_create_preferences_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preferences', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->float('price');
+            $table->string('HotelClass');
+            $table->string('room_type');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preferences');
+    }
+}
